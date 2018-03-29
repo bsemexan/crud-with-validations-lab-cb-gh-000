ef0e18b08f03d7a7ee605b41d3be4fff5c11a857
Berry  Semexan <bsemexan@gmail.com>
2018-03-29 20:01:09 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true
-  validates :released, uniqueness: true
+  validates :title, presence: true, uniqueness: true
+  validates :released
+  
 end
