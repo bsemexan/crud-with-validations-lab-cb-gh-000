9b448f0da7547746707dbca2c94cbb06e247d138
Berry  Semexan <bsemexan@gmail.com>
2018-03-29 20:44:22 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,7 @@ class Song < ActiveRecord::Base
   validates :released, inclusion: { in: [ true, false ] }
   with_options if: :released? do |song|
     song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
   validates :artist_name, presence: true
 
 end
